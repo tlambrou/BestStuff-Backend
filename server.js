8f4130919dcbc3d005740d82ee8f4cee4006e486
Briant Oliveira <briantmoliveira@gmail.com>
2017-11-05 14:36:43 -0800

    some changes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-var path = require('path')
-var express = require('express')
-var app = express()
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// SEQUELIZE
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('beststuff', 'postgres', null, { dialect: 'postgres' });
+
+sequelize
+    .authenticate()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
 
 // RESOURCES
-require('./controllers/contests')(app)
-require('./controllers/items')(app)
-require('./controllers/users')(app)
+require('./controllers/contests')(app);
+require('./controllers/items')(app);
+require('./controllers/users')(app);
 
 // SERVER
 var port = process.env.PORT || 8000;
