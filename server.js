b71451a5fadae58a68adc242ddabc3f2f073bf19
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-06 15:06:48 -0800

    Removed photo and email columns and added jsonwebtoken and bycrypt

--- server.js
+++ server.js
@@ -28,6 +28,5 @@ require('./controllers/users')(app);
 // SERVER
 var port = process.env.PORT || 8000;
 app.listen(port, function () {
-
   console.log('BestStuff Server listening on port 8000!');
 });
