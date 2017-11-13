bb839dbff4c1cf232163f536ac58e399594b76fa
Tassos <lambrou@gmail.com>
2017-11-12 22:37:02 -0800

    Added Items Seeder file

--- seeders/20171113061347-items.js
+++ seeders/20171113061347-items.js
@@ -2,6 +2,11 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+
+    queryInterface.bulkInsert('Items', [
+        { name: "Carne Asada Burrito", createdAt: new Date(), updatedAt: new Date(), id: 10000, contestId: 10000, long: -122.41945609999999, lat: 37.7604843, place_name: 'Taqueria Cancun, Mission Street, San Francisco, CA, United States', place_id: 'ChIJBxm1jjx-j4ARY3l-wL7YQss', voteCount: 124}
+    ], {});
+
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -15,6 +20,8 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Items', null, {});
+
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
