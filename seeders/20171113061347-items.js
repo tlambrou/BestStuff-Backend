16c8da94a1a0c83a94837785bd44ec9e45e33c25
Tassos <lambrou@gmail.com>
2017-11-12 22:39:09 -0800

    Updated items seeder

--- seeders/20171113061347-items.js
+++ seeders/20171113061347-items.js
@@ -4,7 +4,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     queryInterface.bulkInsert('Items', [
-        { name: "Carne Asada Burrito", createdAt: new Date(), updatedAt: new Date(), id: 10000, contestId: 10000, long: -122.41945609999999, lat: 37.7604843, place_name: 'Taqueria Cancun, Mission Street, San Francisco, CA, United States', place_id: 'ChIJBxm1jjx-j4ARY3l-wL7YQss', voteCount: 124}
+        { name: "Carne Asada Burrito", createdAt: new Date(), updatedAt: new Date(), id: 10000, contestId: 10000, long: -122.41945609999999, lat: 37.7604843, place_name: 'Taqueria Cancun, Mission Street, San Francisco, CA, United States', place_id: 'ChIJBxm1jjx-j4ARY3l-wL7YQss', voteCount: 124},
+        { name: "Pollo con Huevos Burrito", createdAt: new Date(), updatedAt: new Date(), id: 10000, contestId: 10000, long: -122.42203589999997, lat: 37.7633976, place_name: 'El Toro Taqueria, Valencia Street, San Francisco, CA, United States', place_id: 'ChIJb-2b7yJ-j4AR8WSRWTxojEw', voteCount: 52}
     ], {});
 
     /*
