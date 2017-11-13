da4e2a84a9bc591aa6f6eb7dac57fcfb7ee3661b
Tassos <lambrou@gmail.com>
2017-11-12 18:20:15 -0800

    Added voteCount to Item Model

---
+++ migrations/20171113021900-addVoteCountToItem.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Items', 'voteCount', {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      }
+    );
+
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Items', 'voteCount');
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
